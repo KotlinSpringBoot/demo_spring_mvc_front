f5c9ff6b4b9922e561cdce4696057b8e730e7b36
jack <guangjian.cgj@alibaba-inc.com>
2018-02-03 22:36:18 +0800

    front end

--- src/pages/home/PageHome.jsx
+++ src/pages/home/PageHome.jsx
@@ -8,28 +8,12 @@ class PageHome extends Component {
 
   constructor(props) {
     super(props, logic);
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(e) {
-    this.dispatch(['updateState', 'search'], {workNo: e.target.value});
-  }
 
   render() {
-    const {state: {data, workNo, empty, loading}, handleChange} = this;
     return (
       <div className="page-home">
-        {/*<input*/}
-        {/*className="kuma-input"*/}
-        {/*onChange={handleChange}*/}
-        {/*placeholder="请输入员工工号"*/}
-        {/*value={workNo}*/}
-        {/*/>*/}
-        {/*<SearchWord workNo={workNo}/>*/}
-        {/*<LogicRender empty={empty} loading={loading}>*/}
-        {/*<SearchData data={data}/>*/}
-        {/*</LogicRender>*/}
-        {/*<User/>*/}
         <LoginForm/>
       </div>
     );
